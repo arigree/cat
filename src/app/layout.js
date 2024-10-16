39312855c10c8391f41af655ff43a684c89f8c7b
arigree <arigree@iu.edu>
2024-10-16 11:32:48 -0400

    nav setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,19 @@
+import Nav from "@/components/Nav";
 import "./globals.css";
+
+export const metadata = {
+  title: "Week 8",
+  description: "App state management",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+          <Nav />
+        {children}
+ 
+      </body>
     </html>
   );
 }
